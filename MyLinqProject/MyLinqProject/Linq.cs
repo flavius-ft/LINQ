cea55eaed69ae0309de139f343c90fb02d30f787
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-03-28 13:50:00 +0200

    All Function

---
+++ MyLinqProject/MyLinqProject/Linq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqProject
+{
+    public static class Linq
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            foreach (var elem in source)
+            {
+                if (elem.Equals(predicate))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
