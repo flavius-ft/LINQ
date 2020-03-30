e0dc65030f955cbfbfa634e327f9d9c053ce7de4
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-03-30 12:25:55 +0300

    MoreTests

--- MyLinqProject/MyLinqProject/LinqTest.cs
+++ MyLinqProject/MyLinqProject/LinqTest.cs
@@ -6,12 +6,21 @@ namespace MyLinqProject
     public class LinqTest
     {
         [Fact]
-        public void CheckIfNumbersAreEven()
+        public void AllMethodNumbersAreEvenReturnTrue()
         {
             int[] evenNumbers = { 2, 4, 6, 8 };
             bool IsEven(int element) => element % 2 == 0;
 
             Assert.True(evenNumbers.All(IsEven));
+        }
+
+        [Fact]
+        public void AllMethodNumbersAreNotEvenReturnFalse()
+        {
+            int[] evenNumbers = { 2, 4, 6, 9 };
+            bool IsEven(int element) => element % 2 == 0;
+
+            Assert.False(evenNumbers.All(IsEven));
         }
 
         [Fact]
@@ -24,7 +33,7 @@ namespace MyLinqProject
         }
 
         [Fact]
-        public void CheckIfAnyElementREspectTheCondition()
+        public void AnyElementRespectTheConditionReturnTrue()
         {
             int[] elements = { 1, 4, 5, 9 };
 
@@ -34,12 +43,49 @@ namespace MyLinqProject
         }
 
         [Fact]
-        public void GetFirstElementWhoIsEven()
+        public void AnyElementDoNotRespectTheConditionReturnFalse()
+        {
+            int[] elements = { 1, 3, 5, 9 };
+
+            bool IsEven(int element) => element % 2 == 0;
+
+            Assert.False(elements.Any(IsEven));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnAnyMethodWhenTheArrayAndPredicatAreNull()
+        {
+            string[] elements = null;
+            bool IsEven(string element) => element == null;
+
+            Assert.Throws<ArgumentNullException>(() => elements.Any(IsEven));
+        }
+
+        [Fact]
+        public void FirstMethodReturnsTheElementWhoIsEven()
         {
             int[] evenNumbers = { 1, 4, 5, 8 };
             bool IsEven(int element) => element % 2 == 0;
 
             Assert.Equal(4, evenNumbers.First(IsEven));
+        }
+
+        [Fact]
+        public void ArgumentNullExceptionOnFirstMethodWhenTheArrayAndPredicatAreNull()
+        {
+            string[] elements = null;
+            bool IsEven(string element) => element == null;
+
+            Assert.Throws<ArgumentNullException>(() => elements.First(IsEven));
+        }
+
+        [Fact]
+        public void FirstElementThrowsInvlaidOperationExceptionWhenNoElementIsEven()
+        {
+            int[] evenNumbers = { 1, 3, 5, 9 };
+            bool IsEven(int element) => element % 2 == 0;
+
+            Assert.Throws<InvalidOperationException>(() => evenNumbers.First(IsEven));
         }
     }
 }
